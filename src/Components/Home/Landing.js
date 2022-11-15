4e032ac2a7ec1ea0be1bd7d95bc29c1558924ab3
Valentina <oorhmo@gmail.com>
2022-11-15 13:42:57 +0100

    Components Config

--- src/Components/Home/Landing.js
+++ src/Components/Home/Landing.js
@@ -1,12 +1,152 @@
+import './Landing.css';
 import React from 'react';
 
-import NavBar from "../../Components/NavBar";
+import One from '../../Images/Nft_Places/5.svg';
+import Two from '../../Images/Nft_Places/6.svg';
+import Three from '../../Images/Nft_Places/7.svg';
+import Four from '../../Images/Nft_Places/8.svg';
+import Five from '../../Images/Nft_Places/1.svg';
+import Six from '../../Images/Nft_Places/2.svg';
+import Seven from '../../Images/Nft_Places/3.svg';
+import Eight from '../../Images/Nft_Places/4.svg';
+
+import { FaStar } from 'react-icons/fa';
 
 const Landing = () => {
+    const cards = [
+      {
+        id:1,
+        src:One
+      },
+      {
+        id:2,
+        src:Two
+      },
+      {
+        id:3,
+        src:Three
+      },
+      {
+        id:4,
+        src:Four
+      },
+      {
+        id:5,
+        src:Five
+      },
+      {
+        id:6,
+        src:Six
+      },
+      {
+        id:7,
+        src:Seven
+      },
+      {
+        id:8,
+        src:Eight
+      },
+    ]
   return (
-    <div className>
-    <NavBar/>
+    <div name="home" className=" w-full">
+
+      <div className="m-16 max-w-screen-xl flex flex-col items-center justify-between h-full  md:flex-row">
+        <div className="flex flex-col justify-center h-full m-6 mx-auto">
+
+          <h2 className=" text-4xl sm-text-xl font-bold text-black"> Rent a <span className="purple">Place</span> away from <br/>
+              <span className="purple">Home</span> in the <span className="purple">Metaverse</span></h2>
+
+              <p className="text-lg text-black py-4 mt-4 ">we provide you access to luxury and affordable houses <br/>
+              in the metaverse get a chance to turn your<br/>
+              imagination to reality at your comfort zone</p>
+
+              <div>
+              <form className="flex">
+                <input
+                  type="text"
+                  name="text"
+                  placeholder="Search for location"
+                  className="text-sm text-white w-fit px-6 py-3 my-2"
+                />
+              <button
+                className="text-white w-fit px-6 py-3 my-2">
+                Search
+              </button>
+              </form>
+              </div>
+          </div>
+          <div className="mt-8 py-4 w-full md:px-12  md:w-1/2">
+            <img
+              src="../../Images/Home/hero-image_1.svg"
+              alt="home"
+              />
+          </div>
+      </div>
+
+      <div className="flex banner  my-4 w-full h-12">
+        <img
+          src="../../Images/Home/MBToken.svg"
+          alt="MBToken"
+          className="w-1/3 my-3 md:my-2"/>
+
+        <img
+          src="../../Images/Home/Metamask.svg"
+          alt="Metamask"
+          className="w-1/3 my-3 md:my-2"/>
+
+        <img
+          src="../../Images/Home/opensea.svg"
+          alt="Opensea"
+          className="w-1/3 my-3 md:my-2"/>
+      </div>
+
+
+      <div className="max-w-screen-xl p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-4">
+        <h3 className="font-bold text-3xl md:text-5xl m-8"> Inspiration for your next adventure</h3>
+        </div>
+
+          <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 sm:px-36 md:px-12 lg:px-4 duration-200 hover:scale=105">
+        {cards.map(({ id, src}) => (
+          <div key={id}>
+            <div className="cursor-pointer box m-2 p-4">
+              <img src={src} alt="" className=" duration-200 hover:scale=105 p-4"/>
+              <div className="flex justify-between mt-2">
+                <span className="text-[10px]">Desert King</span>
+                <p className="text-[10px] font-bold"> 1MBT per night</p>
+              </div>
+              <div className="flex justify-between mt-2">
+                <span className="text-[10px]">2345km away</span>
+                <p className="text-[10px]"> available for 2weeks stay</p>
+              </div>
+              <div className="flex my-2 purple">
+                <FaStar className="mr-1"/>
+                <FaStar className="mx-1"/>
+                <FaStar className="mx-1"/>
+                <FaStar className="mx-1"/>
+                <FaStar className="mx-1"/>
+              </div>
+            </div>
+            </div>
+          ))}
+      </div>
+      </div>
+
+      <div className="purple m-16 max-w-screen-xl flex flex-col items-center justify-between h-full  md:flex-row">
+        <div className="flex flex-col justify-center h-full m-6 mx-auto">
+
+          <h2 className=" text-4xl sm-text-xl font-bold text-black">
+            Metabnb Nfts </h2>
+          </div>
+          <div className="mt-8 py-4 w-full md:px-12  md:w-1/2">
+            <img
+              src="../../Images/Home/hero-image_2.svg"
+              alt="home"
+              />
+          </div>
+      </div>
     </div>
+
   )
 }
 export default Landing;
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
+  const [nav, setNav] = useState(false);
   const links = [
     {
       id: 1,
@@ -21,21 +23,50 @@ const NavBar = () => {
 
   ]
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 fixed">
-      <img src="./Images/Logos/Logo.svg" alt="Metabnb logo"/>
+    <div className="md:px-16 md:py-12 flex justify-between items-center w-full h-20 px-4 fixed bg-white">
+      <div className="p-2">
+      <img
+        src="./Images/Logos/Logo.svg"
+        alt="Metabnb logo"
+        className="w-2/3 md:1/4"/>
+      </div>
 
-      <ul className="flex">
+      <ul className="hidden md:flex">
         {links.map(({ id, link}) => (
           <li
             key={id}
-            className ="px-4 cursor-pointer hover:scale-105 duration 200"
+            className ="text-lg leading-6 px-4 cursor-pointer hover:scale-105 duration 200"
               >
               {link}
               </li>
         ))}
       </ul>
 
-      <button>Connect Wallet</button>
+      <button
+        className="text-white text-sm w-fit px-4 py-1 my-1 rounded-lg">
+      Connect Wallet
+      </button>
+
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-black md:hidden"
+        >
+        {nav ? <FaTimes size={20} /> : <FaBars size={20}/>}
+    </div>
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-white text-black">
+          {links.map(({ id, link}) => (
+            <li
+              key={id}
+              className =" px-4 py-6 text-4xl cursor-pointer hover:scale-105 duration 200"
+                >
+                {link}
+                </li>
+              ))}
+            </ul>
+      )}
+
     </div>
   )
 }
