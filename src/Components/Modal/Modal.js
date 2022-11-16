0d532b290ff2a2e87eea51097bd4556b671e8727
Valentina <98624335+Orhmo@users.noreply.github.com>
2022-11-16 16:21:01 +0100

    Bug fix

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -10,13 +10,14 @@ const Modal = ({open, onClose}) => {
   if (!open) return null
   return (
     <div className="overlay">
-      <div className="modalContainer bg-white px-4 pt-5 pb-4 sm:p-4 sm:pb-4">
+      <div className="modalContainer rounded-lg bg-white px-4 pt-5 pb-4 sm:p-4 sm:pb-4">
         <div className="content">
         <p
           onClick={onClose}
           className="closeBtn ">X</p>
-        <div className="m-2 mb-4 text-center">
-          <h3 className="text-lg font-medium leading-6 text-black">Connect Wallet</h3>
+        <div className="m-2 mb-4">
+          <span className="text-xl font-bold mt-2 mb-4">Connect Wallet</span>
+          <div className="wallet-border"></div>
           <p className="text-sm text-grey-500">Choose your prefarred wallet:</p>
           <div>
             <a href="Button" className="mt-3 border rounded-md bg-white inline-flex border-gray-200 w-full justify-between font-mediun text-black px-4 py-2 shadow-sm hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-offset-2 sm:mt-4 sm:text-sm">
